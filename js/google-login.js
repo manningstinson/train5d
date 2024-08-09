dfe745d0c96a2bf979aa19c4cc7bf45152ced004
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-08-09 15:48:13 +0000

    updated google login

--- js/google-login.js
+++ js/google-login.js
@@ -1,32 +1,7 @@
-function init() {
-  gapi.load("auth2", function () {
-    gapi.auth2
-      .init({
-        client_id:
-          "482824739572-mr45ubm3oghdit3jboigvj87fssco3kh.apps.googleusercontent.com",
-      })
-      .then(
-        function () {
-          console.log("Google Auth initialized");
-        },
-        function (error) {
-          console.error("Error initializing Google Auth:", error);
-        }
-      );
-  });
-}
-
-function signIn() {
-  var googleAuth = gapi.auth2.getAuthInstance();
-  googleAuth.signIn().then(
-    function (googleUser) {
-      console.log("User signed in");
-      console.log(googleUser.getBasicProfile());
-      var id_token = googleUser.getAuthResponse().id_token;
-      console.log("ID Token: " + id_token);
-    },
-    function (error) {
-      console.error("Error signing in", error);
-    }
-  );
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId());
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
 }
